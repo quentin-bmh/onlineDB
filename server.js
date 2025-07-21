8db4fa8c7c11bb26f4700c14ac74a590bf227f25
quentin-bmh <137182152+quentin-bmh@users.noreply.github.com>
2025-07-21 14:46:11 +0200

    dernières modifs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
-
+// Import des routes API
 const voiesRoutes = require('./routes/voies');
 const advRoutes = require('./routes/adv');
 const b2vRoutes = require('./routes/b2v');
 
-
-app.use(express.static('public'));
+// Middlewares de parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,7 +18,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(voiesRoutes);
 app.use(advRoutes);
 app.use(b2vRoutes);
+// Fichiers statiques
+app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`🚀 Serveur Express en ligne : http://localhost:${port}`);
+// Redirection de "/" vers index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
+
+// URL propres pour les pages HTML (ex: /advBoulogne => views/advBoulogne.html)
+app.get('/:page', (req, res, next) => {
+  const filePath = path.join(__dirname, 'public/views', `${req.params.page}.html`);
+  res.sendFile(filePath, err => {
+    if (err) next(); // passe au middleware suivant en cas d'erreur (404)
+  });
+});
+
+// Lancement du serveur
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Serveur lancé sur http://0.0.0.0:${port}`);
+});
+
+
+
+
+
